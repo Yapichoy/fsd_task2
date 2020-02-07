89772fc8663b518b42a2e709890b9dba67f8d8d9
yapichoy <torezracer@gmail.com>
2020-02-07 14:54:52 +0300

    13 add dropdown

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,5 @@
 require("@modules/jquery.maskedinput/src/jquery.maskedinput.js");
-require("vendors/air_datepicker/js/datepicker.js");
+require("vendors/air_datepicker/js/datepicker.min.js");
 $(document).ready(function(){
   $('#date').mask("99.99.9999", {placeholder: "ДД.ММ.ГГГГ" });
   $('#pickup-date').datepicker();
